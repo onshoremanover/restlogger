ef6570fb97552d5cd8aabe0f33b3241659289906
onshoremanover <lukas.a.pellegrini@gmail.com>
2020-12-04 16:59:32 +0100

    pater noster

---
+++ pycli/setup.py
@@ -0,0 +1,11 @@
+from setuptools impoart setup
+setup(
+        name = 'pycli',
+        version = '0.1.0',
+        packages = ['pycli']
+        entry_points = {
+            'console_scripts': [
+                'pycli = pycli.__main__:main'
+                ]
+            })
+
