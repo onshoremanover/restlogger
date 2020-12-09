c9e8e312c7ec2ed03b20bd2804c869c8d12aff77
onshoremanover <lukas.a.pellegrini@gmail.com>
2020-12-09 02:06:26 +0100

    added logging into csv

--- restlogger/__main__.py
+++ restlogger/__main__.py
@@ -1,15 +1,19 @@
 import sys
+import unittest
+import logging
 from .classmodule import My_Class
 from .funcmodule import my_function
 from .class_requests import Request_Class
 from .class_logger import Logger_Class
 from .test_requests import Requests_Test
 
+logging.basicConfig(filename='test.csv', level=logging.DEBUG, format='%(asctime)s;%(levelname)s;%(message)s')
+
 def main():
-    print('====================================')
-    print('in main')
+    logging.debug('====================================')
+    logging.debug('in main')
     args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
+    logging.debug('count of args :: {}'.format(len(args)))
 
     my_function('hello world')
 
@@ -22,8 +26,8 @@ def main():
     my_dict = my_object2.set_request()
     my_object2.parse_json(my_dict)
     
-    print(my_dict['name'])
+    logging.debug(my_dict['name'])
     
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
--- restlogger/funcmodule.py
+++ restlogger/funcmodule.py
@@ -1,3 +1,5 @@
+import logging
+
 def my_function(text_to_display):
-    print('text from my_function :: {}'.format(text_to_display))
+    logging.debug('text from my_function :: {}'.format(text_to_display))
 
