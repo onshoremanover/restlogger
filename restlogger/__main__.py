11b8df2991aadcf17af9d606e40de4a6148acfe9
onshoremanover <lukas.a.pellegrini@gmail.com>
2020-12-15 14:38:43 +0100

    scrolling

--- restlogger/__main__.py
+++ restlogger/__main__.py
@@ -1,10 +1,11 @@
 import sys, getopt, io, re
 import requests, logging, threading, time
-from queue import Queue
 import json, csv
+from datetime import datetime
+
 from .func_arg import my_argument_function
 from .class_logger import My_Logger_Class
-from datetime import datetime
+from .test_requests import Requests_Test
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -27,13 +28,12 @@ def main():
 
     yes_change = re.search("y", argu['change'], flags = re.I)
 
-
     my_object = My_Logger_Class(argu, now)
     my_object.clean_up()
     my_object.set_header()
     
     print("restlogger started logging at {} for every {} seconds".format(now,argu['freq']))
-
+    print(my_object.__str__())
     for _ in range(1,10):
         my_dict = my_object.set_request()
         my_information = my_object.set_content(my_dict)
@@ -44,6 +44,7 @@ def main():
         else:
             my_object.logging_fixed(my_information)
 
+        my_object.scroll()
         time.sleep(argu['freq'])
     
     now = datetime.now()
